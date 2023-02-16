56f509d2ff3c309a7a058d22bc6b5dfc63b2c574
kako07 <kakooluwatosin@gmail.com>
2023-02-16 12:16:21 -0800

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
  * main - Allocating the size of data types
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
-	printf(" char size: %i byte(s)\n", sizeof(char));
-	printf(" int size: %i byte(s)\n", sizeof(int));
-	printf(" long int size: %i byte(s)\n", sizeof(long int));
-	printf(" long long int size: %i byte(s)\n", sizeof(long long int));
-	printf(" float size: %i byte(s)\n", sizeof(float));
+	printf("size of a chart: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
