225a586c5df0fa1c386f68a7c8ba2cc95a7145ea
kako07 <kakooluwatosin@gmail.com>
2023-02-16 11:17:06 -0800

    Data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main - size of various datatypes
+ * main - Allocating the size of data types
  * Return: 0 (success)
  */
 int main (void)
 {
-	printf("char size: %lu byte(s)\n", sizeof(char));
-	printf("int size: %lu byte(s)\n", sizeof(int));
-	printf("long int size: %lu byte(s)\n", sizeof(long int));
-	printf("long long int size: %lu byte(s)\n", sizeof(long long int));
-	printf("float size: %lu byte(s)\n", sizeof(float));
-	return (0);
+printf(" char size: %lu bytes\n", sizeof(char));
+printf(" int size: %lu bytes\n", sizeof(int));
+printf(" long int size: %lu bytes\n", sizeof(long int));
+printf(" long long int size: %lu bytes\n", sizeof(long long int));
+printf(" float size: %lu bytes\n", sizeof(float));
 }
