8ab7cd890f7edcdaae44e469924965866577284d
kako07 <kakooluwatosin@gmail.com>
2023-02-16 12:08:34 -0800

    Data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,10 @@
  */
 int main (void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-printf(" char size: %lu bytes\n", (unsigned long)sizeof(a));
-printf(" int size: %lu bytes\n", (unsigned long)sizeof(b));
-printf(" long int size: %lu bytes\n", (unsigned long)sizeof(c));
-printf(" long long int size: %lu bytes\n", (unsigned long)sizeof(d));
-printf(" float size: %lu bytes\n", (unsigned long)sizeof(e));
+	printf(" char size: %i byte(s)\n", sizeof(char));
+	printf(" int size: %i byte(s)\n", sizeof(int));
+	printf(" long int size: %i byte(s)\n", sizeof(long int));
+	printf(" long long int size: %i byte(s)\n", sizeof(long long int));
+	printf(" float size: %i byte(s)\n", sizeof(float));
+	return (0);
 }
