ea1cf68c495ce93af2a5f17b0886fefe28c435c0
kako07 <kakooluwatosin@gmail.com>
2023-02-16 13:09:38 -0800

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print a line with a return
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
