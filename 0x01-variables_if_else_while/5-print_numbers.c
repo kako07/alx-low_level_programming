09b777f7bd8d079058939f1f72ad0f1debb9ddf3
kako07 <kakooluwatosin@gmail.com>
2023-02-19 04:23:07 -0800

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * prints number from 0 to 9
+ * Return: Alwaya 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
