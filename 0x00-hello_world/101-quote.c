ff15312df0e66db1a2acb0eda27f8a3b2935ed92
kako07 <kakooluwatosin@gmail.com>
2023-02-19 14:51:31 -0800

    unix is basic

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - print a line 
+ * main - Entry point
  * Return: Always 1 (success)
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	return(1);
 }
