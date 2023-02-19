91fbe56ac0e47164a9b0aff699cf84f251752f2c
kako07 <kakooluwatosin@gmail.com>
2023-02-19 06:18:50 -0800

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single numbers of base 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		putchar(a + '0');
+		n++
+	}
+	putchar('\n');
+	return (0);
+}
