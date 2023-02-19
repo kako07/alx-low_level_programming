4e5fe9e691e489a66966dc030cabd1ae9ef2e952
kako07 <kakooluwatosin@gmail.com>
2023-02-19 02:43:05 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c[26] = "abcdefghijklmnopqrstuvwxyz"
+
+		int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
