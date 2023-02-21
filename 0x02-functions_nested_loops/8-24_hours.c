36fddc397f62e0ec68f43cfa70464aa137cedd0d
kako07 <kakooluwatosin@gmail.com>
2023-02-21 11:21:54 -0800

    printing time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
