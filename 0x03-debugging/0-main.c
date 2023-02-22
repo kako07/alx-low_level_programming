f46028c571ae08961ae872f1002ee442614d8305
kako07 <kakooluwatosin@gmail.com>
2023-02-22 08:25:38 -0800

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - prints positive or negative output
+ * @i : parameter
+ * Return: Always 0 (success)
+ */
+
+void positive_or_negative(int i)
+{
+	i = 0;
+	return (positive_or_negative);
+}
