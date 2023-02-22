b818673f16bb3192dc1b0909563c1f77ec61e32d
kako07 <kakooluwatosin@gmail.com>
2023-02-21 18:38:13 -0800

    sum two intergers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,12 @@
 /**
  * add - prints sum of two intergers
  * @a  : parameter to check
+ * @b : parameter to check
  * Return: Always 0 (success)
  */
 
 int add(int a, int b)
 {
 	int sum = a + b;
-
-	_putchar(sum);
 	return (sum);
 }
