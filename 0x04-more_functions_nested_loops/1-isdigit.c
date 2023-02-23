67f09c6b90eecafa8763677b9dcdf4f36c0a2056
kako07 <kakooluwatosin@gmail.com>
2023-02-23 08:24:48 -0800

    digit 0 - 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit 0 - 9
+ * @c : parameter to cheeck
+ * Return: Always 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
