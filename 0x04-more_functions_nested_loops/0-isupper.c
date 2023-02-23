ffa77611d079a47faa5abae93df94d37243e0443
kako07 <kakooluwatosin@gmail.com>
2023-02-23 08:01:24 -0800

    Uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c : parameter to check
+ * Return: Always 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' || a <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
