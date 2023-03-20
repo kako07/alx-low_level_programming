dce7798106470e9f893ab5e93aa230b20919abec
kako07 <kakooluwatosin@gmail.com>
2023-03-20 04:47:05 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 #define NAME __FILE__
 
 /**
- * main - prints name of file
- * Return : Always 0 success
- */
+	* main - Prints name of file
+	* Return: Always 0(success)
+	*/
 int main(void)
 {
 	printf("%s\n", NAME);
